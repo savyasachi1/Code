e45dd7a13689de67f6ece8cc600762ec46537b75
Savya Sachi <savyasachifl@gmail.com>
2023-08-01 19:37:54 +0530

    Singly link list

--- Linked list/singlyRev.cpp
+++ Linked list/singlyRev.cpp
@@ -1,7 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello World!";
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int a)
+    {
+        this->data = a;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *head, int d){
+    Node* temp = new Node(d);
+    temp-> next = head;
+    head =temp; 
+}
+
+void print(Node* head){
+    Node* temp = head;
+    while (temp!=NULL){
+        cout<<temp-> data<<" ";
+        temp = temp-> next;
+    }
+}
+
+int main()
+{
+    // cout<<"Hello World!";
+    Node *node1 = new Node(10);
+    cout << node1->data << endl;
+    cout << node1->next << endl;
+
     return 0;
 }
